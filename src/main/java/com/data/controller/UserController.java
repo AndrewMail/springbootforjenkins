ec11be9c533518c4798fc92882bac9346c670622
andrew <1225811647@qq.com>
2020-03-15 15:54:46 +0800

    add queryusername ok123

--- src/main/java/com/data/controller/UserController.java
+++ src/main/java/com/data/controller/UserController.java
@@ -22,7 +22,7 @@ public class UserController {
 
     @GetMapping("/queryusername/{id}")
     public String queryUserName(@PathVariable("id") Long id){
-        return "id:"+id+",name:Jack,ok";
+        return "id:"+id+",name:Jack,ok123";
     }
 
     @PostMapping
